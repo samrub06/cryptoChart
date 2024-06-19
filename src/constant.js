7c29e301694496a41a5c27f347c7277cad1936d3
Samuel Charbit <76777361+samrub06@users.noreply.github.com>
2024-06-19 23:40:51 +0300

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
 export const cryptoUrl =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=falsecategories &x_cg_demo_api_key=CG-YoRMorw84Hfac5sXzTM2YoHG";
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=falsecategories";
